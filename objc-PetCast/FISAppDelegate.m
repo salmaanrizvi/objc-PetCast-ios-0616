c96fd46fceb23491f66c4ac66db23a4735851634
salmaanrizvi <sar228@cornell.edu>
2016-06-15 16:28:09 -0400

    all tests passed, casted successfully

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,25 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISDog.h"
+#import "FISPet.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISDog *dog = [[FISDog alloc] init];
+    NSLog(@"%@", [dog makeASound]);
+    NSLog(@"%@", [dog assaultTheMailman]);
+    
+    FISPet *pet = (FISPet *)dog;
+    NSLog(@"%@", [pet makeASound]);
+    NSLog(@"%@", [(FISDog *)pet assaultTheMailman]);
+
+    FISPet *neoPet = [[FISPet alloc] init];
+    NSLog(@"%@", [(FISDog *)neoPet makeASound]);
+    //NSLog(@"%@", [(FISDog *)neoPet assaultTheMailman]);
+    
     return YES;
 }
 
---
+++ objc-PetCast/FISPet.m
@@ -0,0 +1,21 @@
+//
+//  FISPet.m
+//  objc-PetCast
+//
+//  Created by Salmaan Rizvi on 6/15/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+@implementation FISPet
+
+-(NSString *)makeASound {
+    return @"Pet me!";
+}
+
+-(NSString *)eatSomething {
+    return @"Nom nom nom.";
+}
+
+@end
